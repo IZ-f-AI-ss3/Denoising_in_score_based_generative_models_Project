a070751c12e396ed1cefff6923b4b3f5ffab9a24
IZFAISSAL <faissal.izermine@eleves.enpc.fr>
2025-11-24 10:34:37 +0100

    more lib mismatch errors

--- main.py
+++ main.py
@@ -34,11 +34,15 @@ def parse_args_and_config():
     # parse config file
     if not args.test:
         with open(os.path.join('configs', args.config), 'r') as f:
-            config = yaml.load(f)
+            # config = yaml.load(f)
+            config = yaml.load(f, Loader=yaml.FullLoader)
+
         new_config = dict2namespace(config)
     else:
         with open(os.path.join(args.log, 'config.yml'), 'r') as f:
-            config = yaml.load(f)
+            # config = yaml.load(f)
+            config = yaml.load(f, Loader=yaml.FullLoader)
+
         new_config = config
 
     if not args.test:
