27c0aceafe2d06d0cb95ad3b055b0a319b168926
IZFAISSAL <faissal.izermine@eleves.enpc.fr>
2025-12-17 13:12:48 +0100

    400 batch_size

--- runners/anneal_runner.py
+++ runners/anneal_runner.py
@@ -356,7 +356,7 @@ class AnnealRunner():
         score.eval()
 
         n_samples = self.args.n_samples
-        batch_size = 100
+        batch_size = 400
 
         sampling_method = self.args.sampling_type
 
